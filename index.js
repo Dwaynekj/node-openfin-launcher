a22c0b68f72434a75ec1207ba59cbac8f7870506
Dwayne Jones <dwayne@openfin.co>
2016-03-03 15:04:07 -0500

    Work thus far

--- index.js
+++ index.js
@@ -15,68 +15,36 @@ var path = require('path'),
     fs = require('fs'),
     _ = require('lodash'),
     q = require('q'),
-    Registry = require('winreg'),
-    HKCURegKey,
-    HKLMRegKey,
-    regKeyVals = [],
     xpLocation = '\\Local Settings\\Application Data\\OpenFin',
     eightOrGreater = '\\AppData\\Local\\OpenFin', //at least windows 8 or greater
-    defaultAppData;
+    defaultAppData = path.join(process.env['USERPROFILE'], isXP ? xpLocation : eightOrGreater),
+    defaultOptions,
+    Registry = require('winreg');
 
-//Read the Registry for where the RVM should be installed
-HKCURegKey = new Registry({
-      hive: Registry.HKCU,  // open registry hive HKEY_CURRENT_USER
-      key:  '\\Software\\Openfin\\RVM\\installDir'
-  });
+function readRegistry(registryAcronym){
+    var deferred = q.defer(),
+        regKey = new Registry({
+              hive: Registry[registryAcronym],
+              key:  '\\Software\\Openfin\\RVM\\installDir'
+          });
 
-HKLMRegKey = new Registry({
-    hive: Registry.HKLM, // open registry hive HKEY_LOCAL_MACHINE
-    key:  '\\Software\\Openfin\\RVM\\installDir'
-});
-
-HKCURegKey.values(function (err, items) {
-    if (err) {
-        console.log('ERROR: '+err);
-    } else {
-        for (var i=0; i<items.length; i++) {
-            console.log('ITEM: '+items[i].name+'\t'+items[i].type+'\t'+items[i].value);
+    regKey.values(function (err, items) {
+        if (err) {
+            //console.log(registryAcronym +' NOT FOUND: '+err);
+            deferred.reject(err);
+        } else {
+            if (items.length > 0){
+                deferred.reject();
+            }
+            deferred.resolve(items);
         }
-        regKeyVals = items;
-    }
-});
-
-// HKLM Reg values take precendence over HKCU
-HKLMRegKey.values(function (err, items) {
-    if (err) {
-        console.log('ERROR: '+err);
-    } else {
-        for (var i=0; i<items.length; i++) {
-            console.log('ITEM: '+items[i].name+'\t'+items[i].type+'\t'+items[i].value);
-        }
-        //Overwrite the HKCU
-        if (items.length > 0){
-            regKeyVals = items;
-        }
-    }
-});
-
-if (regKeyVals[0] && regKeyVals[0].value) {
-    defaultAppData = regKeyVals[0].value;
-} else {
-    // this is equivalent to %localappdata%\OpenFin
-    defaultAppData = path.join(process.env['USERPROFILE'], isXP ? xpLocation : eightOrGreater);
+    });
+    return deferred.promise;
 }
-var defaultOptions = {
-    rvmPath: path.resolve(defaultAppData, 'OpenFinRVM.exe'),
-    rvmUrl: 'https://developer.openfin.co/release/rvm/latestVersion', //Get the latest Version
-    rvmGlobalCommand: null //this is undocumented, do we still need it?
-};
 
 function launchOpenFin(options) {
-    var deffered = q.defer();
-
-    // use the options, filling in the defaults without clobbering them
-    var combinedOpts = _.defaults(_.clone(options), defaultOptions);
+    var deffered = q.defer(),
+        combinedOpts;
 
     function launch() {
         fs.exists(path.resolve(combinedOpts.rvmPath), function(exists) {
@@ -105,8 +73,56 @@ function launchOpenFin(options) {
         });
     }
 
-    if (os.type().toLowerCase().indexOf('windows') > -1) {
-        launch();
+    if (isWindows) {
+        //Read the Registry for where the RVM should be installed
+        var HKLMRegistryPromise = readRegistry('HKLM'), // open registry hive HKEY_LOCAL_MACHINE
+            HKCURegistryPromise = readRegistry('HKCU'), // open registry hive HKEY_CURRENT_USER
+            registryValue;
+
+        // HKLM Reg values take precendence over HKCU
+        // hence they are passed first in the subsequent array
+        //q respects the order of the promises called in the returned result
+        q.allSettled([HKLMRegistryPromise, HKCURegistryPromise])
+        .then(function(results){
+            //Returns the Registry keys with the HKLM values first if available
+            var keys = [];
+            results.forEach(function (result) {
+                if (result.state === 'fulfilled') {
+                    // This item was loaded!
+                    //console.log(result.value);
+                    keys.push(result.value);
+                } else {
+                    // This item failed to be loaded :(
+                    //console.log(result.reason);
+                }
+            });
+
+            if (keys.length > 0){
+                keys = _.flattenDeep(keys);
+                registryValue = _.find(keys, function (k) {
+                    return !!k && (typeof k === "string") && k.length > 0;
+                });
+
+                if (registryValue) {
+                    defaultAppData = registryValue;
+                }
+            } else {
+                console.log("No registry values found in HKEY_LOCAL_MACHINE or HKEY_CURRENT_USER");
+            }
+
+            defaultOptions = {
+                rvmPath: path.resolve(defaultAppData, 'OpenFinRVM.exe'),
+                rvmUrl: 'https://developer.openfin.co/release/rvm/latest', //Get the latest Version
+                rvmGlobalCommand: null //this is undocumented, do we still need it?
+            };
+
+            // use the options, filling in the defaults without clobbering them
+            combinedOpts = _.defaults(_.clone(options), defaultOptions);
+            console.log('RVM install location set to: ' + combinedOpts.rvmPath);
+
+            launch();
+        });
+
     } else {
         deffered.reject(new Error(nonSupportedOSMessage));
     }
